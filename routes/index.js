30fc00c38c91aa952b2d1b85353b81ba16fcd45c
Venilton FalvoJr <falvojr@gmail.com>
2017-02-27 13:56:06 -0300

    Ajustes finais: Nome e Fonte

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Req-mLearning' });
+  res.render('index', { title: 'ReqML-Catalog' });
 });
 
 module.exports = router;
